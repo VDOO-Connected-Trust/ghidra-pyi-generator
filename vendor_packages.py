ac0d7098b69764a20fef64a04ef2b9a01250a2e9
Tamir Bahar <tamir.bahar@gmail.com>
2024-10-04 08:57:41 +0300

    And now?

--- vendor_packages.py
+++ vendor_packages.py
@@ -3,4 +3,4 @@ import shutil
 import os.path
 
 os.system("mkdir -p %s" % (os.path.dirname(site.getsitepackages()[0]), ))
-shutil.copytree(os.path.join(__file__, "..", "vendor"), site.getsitepackages()[0])
+shutil.copytree(os.path.join(os.path.dirname(__file__), "vendor"), site.getsitepackages()[0])
